4c9ab36bfbb41bbe80a45eb1cfe5ff53ed3f826a
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2021-05-04 23:41:34 +0530

    cardlist reusable component

---
+++ src/components/CardList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { fade, makeStyles, withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  item: {
+    margin: theme.spacing(1, 1),
+    borderRadius: theme.spacing(1.25),
+    "&:hover": {
+      boxShadow: theme.shadows[2],
+    },
+  },
+}));
+
+export default function CardList({
+  card: Card,
+  data = [...new Array(27)],
+  getLink = () => "",
+}) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      {data.map((o, i) => (
+        <Link key={i.toString()} className={classes.item} to={getLink(o)}>
+          <Card {...o} />
+        </Link>
+      ))}
+    </div>
+  );
+}
